34cd1da2329c8b5f458016531c96bc49b2a6912e
Arie May Wibowo <ariemayw@gmail.com>
2020-03-04 17:35:27 +0700

    Initial Commit

---
+++ GetSuccess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetSuccess
+//
+//  Created by Arie May Wibowo on 04/03/20.
+//  Copyright © 2020 Arie May Wibowo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
