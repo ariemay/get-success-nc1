9aedbad322a6fb2c09b52eec2b8365ec53d638d9
Arie May Wibowo <ariemayw@gmail.com>
2020-03-08 09:26:27 +0700

    nearly done

---
+++ GetSuccess/MotivationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MotivationViewController.swift
+//  GetSuccess
+//
+//  Created by Arie May Wibowo on 08/03/20.
+//  Copyright © 2020 Arie May Wibowo. All rights reserved.
+//
+
+import UIKit
+
+class MotivationViewController: UIViewController {
+
+    @IBOutlet weak var imageQuotes: UIImageView!
+    @IBOutlet weak var textQuotes: UITextView!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    let images: [UIImage] = [#imageLiteral(resourceName: "elon1"), #imageLiteral(resourceName: "new2")]
+    let textQuote: [String] = [
+        "When something is important enough, you do it even if the odds are not in your favor.",
+        "Life is good...."
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func changeStories(_ sender: UIButton) {
+        var index = Int.random(in: 0...1)
+        imageQuotes.image = images[index]
+        textQuotes.text = textQuote[index]
+    }
+    
+
+}
